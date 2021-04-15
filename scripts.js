a5ec0eb17a568fce3b6e0d1cdf0cf65b084bd040
Belashab <belashabb@gmail.com>
2021-04-15 16:37:13 +0300

    Ticker upgrade with js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
 // Если есть какие-либо предложения по организации кода, то лучше их записывать сразу в мейн и растаскивать по остальным веткам
+
+var ticker = document.querySelector('.ticker')
+   list = document.querySelector('.ticker__list')
+   clone = list.cloneNode(true)
+
+ticker.append(clone)
