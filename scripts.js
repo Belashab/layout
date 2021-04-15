f787de07e7736434dd28eee51eb32a889e1a99b4
Belashab <belashabb@gmail.com>
2021-04-15 17:45:36 +0300

    No ticker but finished Customers and Achievements blocks

--- scripts.js
+++ scripts.js
@@ -1,7 +1,1 @@
 // Если есть какие-либо предложения по организации кода, то лучше их записывать сразу в мейн и растаскивать по остальным веткам
-
-var ticker = document.querySelector('.ticker')
-   list = document.querySelector('.ticker__list')
-   clone = list.cloneNode(true)
-
-ticker.append(clone)
