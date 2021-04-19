d1184312475dcf6779d12fa4cca03cb9f81cb80f
Belashab <belashabb@gmail.com>
2021-04-19 13:31:44 +0300

    first attempts at js positioning text

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 // Если есть какие-либо предложения по организации кода, то лучше их записывать сразу в мейн и растаскивать по остальным веткам
+
+
+// Позиционирование текста по координатам
+
+'use strict'
+
+class Text_over_image {
+    constructor (parent_name, div_name, x, y, w, h) {
+        let div_name = document.getElementById();
+        let parent_name = div_name.parentNode.id;
+    }
+    text_positioning() {
+        div_name.style.position = 'absolute';
+        div_name.style.left = x + 'px';
+        div_name.style.top = y + 'px';
+        div_name.style.width = w + 'px';
+        div_name.style.height = h + 'px';
+        document.parent_name.appendChild (div_name);
+        return div_name;
+    }
+};
+
